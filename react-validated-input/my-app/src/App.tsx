e9ff58ceee5d85d01a45f73492d5050813345dd2
Jesus Rodriguez-Luna <JesusRodriguezLuna3616@gmail.com>
2024-10-20 08:30:07 +0000

    Completed react-validated-input

--- react-validated-input/my-app/src/App.tsx
+++ react-validated-input/my-app/src/App.tsx
@@ -1,8 +1,35 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import { ValidatedInput } from './ValidatedInput';
+import './App.css';
 
 function App() {
+  const [passwordValid, setPasswordValid] = useState(false);
+  const [validationMessage, setValidationMessage] = useState(
+    'A password is required.'
+  );
 
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const userInput = e.target.value;
+
+    if (userInput.length === 0) {
+      setValidationMessage('A password is required.');
+      setPasswordValid(false);
+    } else if (userInput.length < 8) {
+      setValidationMessage('Your password is too short.');
+      setPasswordValid(false);
+    } else {
+      setValidationMessage('');
+      setPasswordValid(true);
+    }
+  }
+
+  return (
+    <ValidatedInput
+      active={passwordValid}
+      label={validationMessage}
+      onChange={handleChange}
+    />
+  );
 }
 
-export default App
+export default App;
---
+++ react-validated-input/my-app/src/ValidatedInput.tsx
@@ -0,0 +1,34 @@
+import { FaCheck } from 'react-icons/fa';
+import { FaXmark } from 'react-icons/fa6';
+
+type Props = {
+  label: string;
+  active: boolean;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+export function ValidatedInput({ label, active, onChange }: Props) {
+  return (
+    <div>
+      <label htmlFor="password">
+        <span className="relative right-13 bottom-2">Password</span>
+        <div className="flex items-center">
+          <input
+            type="text"
+            className="mr-1"
+            id="password"
+            onChange={onChange}
+          />
+          <FaCheck className={`text-green-400 ${active ? '' : 'sr-only'}`} />
+          <FaXmark className={`text-red-600 ${active ? 'sr-only' : ''}`} />
+        </div>
+        <div
+          className={`relative right-2 top-2 text-red-600 ${
+            !active ? '' : 'sr-only'
+          }`}>
+          {label}
+        </div>
+      </label>
+    </div>
+  );
+}
