f969a21047a2686e696acdf915624cda62f4292a
Jesus Rodriguez-Luna <JesusRodriguezLuna3616@gmail.com>
2024-10-25 07:41:56 +0000

    react-multiple-components setup

---
+++ react-multiple-components/my-app/src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+import { RotatingBanner } from './RotatingBanner';
+
+function App() {
+
+  const items = [
+    'Aardvark',
+    'Bengal',
+    'Caterpillar',
+    'Dromedary',
+    'Elephant',
+    'Ferret',
+  ];
+
+  return <RotatingBanner items={items} />
+}
+
+export default App
---
+++ react-multiple-components/my-app/src/RotatingBanner.tsx
@@ -0,0 +1,7 @@
+type Props ={
+  items: string[],
+}
+
+export function RotatingBanner({ items }: Props) {
+  return items;
+}
