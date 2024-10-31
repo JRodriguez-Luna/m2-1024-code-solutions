ec200b923456795c7f1d0157ca761922f448d15e
Jesus Rodriguez-Luna <JesusRodriguezLuna3616@gmail.com>
2024-10-31 01:02:54 +0000

    Completed react-accordion

--- react-accordion/my-app/src/Accordion.tsx
+++ react-accordion/my-app/src/Accordion.tsx
@@ -1,26 +1,22 @@
-import { TopicCard } from "./TopicCard";
+import { useState } from 'react';
+import { TopicCard } from './TopicCard';
 
-const topics = [
-  {
-    id: 1,
-    title: 'Hypertext Markup Language',
-    content:
-      'Hypertext Markup Language (HTML) is the standard markup language for documents designed to be displayed in a web browser. It can be assisted by technologies such as Cascading Style Sheets (CSS) and scripting languages such as JavaScript.',
-  },
-  {
-    id: 3,
-    title: 'Cascading Style Sheets',
-    content:
-      'Cascading Style Sheets (CSS) is a style sheet language used for describing the presentation of a document written in a markup language like HTML. CSS is a cornerstone technology of the World Wide Web, alongside HTML and JavaScript.',
-  },
-  {
-    id: 9,
-    title: 'JavaScript',
-    content:
-      'JavaScript (/ˈdʒɑːvəˌskrɪpt/), often abbreviated as JS, is a high-level, interpreted scripting language that conforms to the ECMAScript specification. JavaScript has curly-bracket syntax, dynamic typing, prototype-based object-orientation, and first-class functions.',
-  },
-];
+type Props = {
+  topics: {
+    id: number;
+    title: string;
+    content: string;
+  }[];
+};
 
-export function Accordion() {
-  return <TopicCard />
+export function Accordion({ topics }: Props) {
+  const [topic, setTopic] = useState<number | null>(null);
+
+  function handleClick(id: number) {
+    setTopic((prevId) => (prevId === id ? null : id));
+  }
+
+  return (
+    <TopicCard topics={topics} onClick={handleClick} selectedTopic={topic} />
+  );
 }
--- react-accordion/my-app/src/TopicCard.tsx
+++ react-accordion/my-app/src/TopicCard.tsx
@@ -1,20 +1,24 @@
 import './TopicCard.css';
 
-export function TopicCard() {
-  return (
-    <div>
-      <div>
-        <h2 className="box">Hypertext Markup Language</h2>
-        <p className='details'>Hypertext Markup Language (HTML) is the standard markup language for documents designed to be displayed in a web browser. It can be assisted by technologies such as Cascading Style Sheets (CSS) and scripting languages such as JavaScript.</p>
-      </div>
-      <div>
-        <h2 className='box'>Cascading Style Sheets</h2>
-        <p className='details hidden'>Showing CSS</p>
-      </div>
-      <div>
-        <h2 className='box'>JavaScript</h2>
-        <p className='details hidden'>Showing JS</p>
-      </div>
+type Props = {
+  topics: {
+    id: number;
+    title: string;
+    content: string;
+  }[];
+  onClick: (id: number) => void;
+  selectedTopic: number | null;
+};
+
+export function TopicCard({ topics, selectedTopic, onClick }: Props) {
+  const box = topics.map((topic) => (
+    <div key={topic.id} onClick={() => onClick(topic.id)}>
+      <h2 className="box">{topic.title}</h2>
+      <p className={`details ${selectedTopic === topic.id ? '' : 'hidden'}`}>
+        {topic.content}
+      </p>
     </div>
-  )
+  ));
+
+  return <div>{box}</div>;
 }
