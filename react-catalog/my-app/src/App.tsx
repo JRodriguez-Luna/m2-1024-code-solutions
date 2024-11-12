09894659bcddce8b055953f33446f4136a2dcef7
Jesus Rodriguez-Luna <JesusRodriguezLuna3616@gmail.com>
2024-11-12 18:02:27 +0000

    added NotFound and dummy About page

--- react-catalog/my-app/src/App.tsx
+++ react-catalog/my-app/src/App.tsx
@@ -2,6 +2,8 @@ import { Header } from './components/Header';
 import { Catalog } from './pages/Catalog';
 import { Route, Routes } from 'react-router-dom';
 import { Details } from './pages/Details';
+import { About } from './pages/About';
+import { NotFound } from './pages/NotFound';
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Route path="/" element={<Header />}>
         <Route index element={<Catalog />} />
         <Route path="details/:productId" element={<Details />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
   );
---
+++ react-catalog/my-app/src/pages/About.tsx
@@ -0,0 +1,7 @@
+export function About() {
+  return (
+    <div className="mx-5 my-2 pt-5 text-3xl w-100 flex justify-center items-center">
+      <p>About</p>
+    </div>
+  );
+}
