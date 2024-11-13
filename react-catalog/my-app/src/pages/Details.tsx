ba8d484d9f3e6a3654452355edf86f4b941d9794
Jesus Rodriguez-Luna <JesusRodriguezLuna3616@gmail.com>
2024-11-13 02:48:33 +0000

    Added 'add to cart' alert

--- react-catalog/my-app/src/pages/Details.tsx
+++ react-catalog/my-app/src/pages/Details.tsx
@@ -22,28 +22,37 @@ export function Details() {
   }, [productId]);
 
   return (
-    <div className="mx-5 my-2 p-4 border w-auto h-auto">
-      <div className="pb-4">
-        <Link to="/" className="">
-          &lt; Back to Catalog
-        </Link>
-      </div>
-      <div>
-        <div className="flex flex-wrap justify-start h-auto ">
-          <img src={product?.imageUrl} alt={product?.name} className="w-96" />
-          <div className="flex w-50 pl-5 flex-col">
-            <div className="text-lg">
-              <h2 className="text-3xl font-semibold pb-1">{product?.name}</h2>
-              <p className="text-gray-400 pb-1">
-                {product?.price ? toDollars(product?.price) : '0.00'}
-              </p>
-              <p>{product?.shortDescription}</p>
+    <div className="mx-5 my-2">
+      <div className="p-4 border w-auto h-auto">
+        <div className="pb-4">
+          <Link to="/" className="">
+            &lt; Back to Catalog
+          </Link>
+        </div>
+        <div>
+          <div className="flex flex-wrap justify-start h-auto ">
+            <img src={product?.imageUrl} alt={product?.name} className="w-96" />
+            <div className="flex w-50 pl-5 flex-col">
+              <div className="text-lg">
+                <h2 className="text-3xl font-semibold pb-1">{product?.name}</h2>
+                <p className="text-gray-400 pb-1">
+                  {product?.price ? toDollars(product?.price) : '0.00'}
+                </p>
+                <p>{product?.shortDescription}</p>
+              </div>
             </div>
           </div>
+          <div className="text-base pt-5">
+            <p>{product?.longDescription}</p>
+          </div>
         </div>
-        <div className="text-base pt-5">
-          <p>{product?.longDescription}</p>
-        </div>
+      </div>
+      <div>
+        <button
+          className="w-auto bg-gray-200	 p-2 rounded"
+          onClick={() => alert(`Added ${product?.name} to Cart `)}>
+          Add to Cart
+        </button>
       </div>
     </div>
   );
