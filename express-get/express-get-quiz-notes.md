622f5975d079e8b813c865bb3f3647695ae0d8c4
Jesus Rodriguez-Luna <JesusRodriguezLuna3616@gmail.com>
2024-12-12 13:58:18 +0000

    Completed express-get

--- express-get/express-get-quiz-notes.md
+++ express-get/express-get-quiz-notes.md
@@ -6,8 +6,9 @@ Answer the following questions in the provided markdown file before turning in t
 
 - What is the purpose of the HTTP `GET` method?
 
+  > For HTTP servers to get and return data.
+
 - What is the appropriate `Content-Type` header for HTTP messages that contain JSON in their bodies?
+  > application/json; charset=utf-8
 
 ## Notes
-
-
--- express-get/server.ts
+++ express-get/server.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import pg from 'pg';
-import { errorMiddleware } from './lib/index.js';
+import { ClientError, errorMiddleware } from './lib/index.js';
 
 const db = new pg.Pool({
   connectionString: 'postgres://dev:dev@localhost/pagila',
@@ -11,6 +11,61 @@ const db = new pg.Pool({
 });
 
 const app = express();
+
+app.get('/api/films', async (req, res, next) => {
+  try {
+    const sql = `
+      select
+        "title",
+        "description",
+        "filmId",
+        "replacementCost"
+      from "films"
+      order by "replacementCost" desc
+      limit 2;
+    `;
+    const result = await db.query(sql);
+    const films = result.rows;
+    if (!films) {
+      throw new ClientError(404, 'films not found.');
+    }
+    res.json(films);
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.get('/api/films/:filmId', async (req, res, next) => {
+  try {
+    const { filmId } = req.params;
+    if (
+      Number.isNaN(filmId) ||
+      !Number.isInteger(+filmId) ||
+      Number(filmId) < 1
+    ) {
+      throw new ClientError(400, 'filmId is required.');
+    }
+    const sql = `
+      select
+        "title",
+        "description",
+        "filmId",
+        "replacementCost"
+      from "films"
+      where "filmId" = $1;
+    `;
+
+    const params = [filmId];
+    const result = await db.query(sql, params);
+    const [film] = result.rows;
+    if (!film) {
+      throw new ClientError(404, `filmId ${filmId} not found.`);
+    }
+    res.json(film);
+  } catch (err) {
+    next(err);
+  }
+});
 
 app.use(errorMiddleware);
 
