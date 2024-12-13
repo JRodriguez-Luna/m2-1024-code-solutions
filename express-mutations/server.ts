2ff1784e025a487d98d852688762be8e2b6f317d
Jesus Rodriguez-Luna <JesusRodriguezLuna3616@gmail.com>
2024-12-13 02:46:28 +0000

    Exercise 1 create

--- express-mutations/server.ts
+++ express-mutations/server.ts
@@ -11,6 +11,33 @@ const db = new pg.Pool({
 });
 
 const app = express();
+app.use(express.json());
+
+app.post('/api/actors', async (req, res, next) => {
+  try {
+    const { firstName, lastName } = req.query;
+    if (!firstName) {
+      throw new ClientError(400, 'firstName is required.');
+    }
+    if (!lastName) {
+      throw new ClientError(400, 'lastName is required.');
+    }
+    const sql = `
+      insert into "actors" ("firstName", "lastName")
+      values ($1, $2)
+      returning *;
+    `;
+    const params = [firstName, lastName];
+    const result = await db.query(sql, params);
+    const [actor] = result.rows;
+    if (!actor) {
+      throw new ClientError(404, 'actor is required.');
+    }
+    res.status(201).json(actor);
+  } catch (err) {
+    next(err);
+  }
+});
 
 // Endpoint for testing
 app.get('/api/actors/:actorId', async (req, res, next) => {
