c0433e777545bc32e1ff679a402e6725b383dd66
Jesus Rodriguez-Luna <JesusRodriguezLuna3616@gmail.com>
2024-12-15 12:37:52 +0000

    Exercise pt.1 complete

---
+++ express-crud/server.ts
@@ -0,0 +1,35 @@
+import pg from 'pg';
+import express from 'express';
+import { errorMiddleware } from './lib';
+
+const db = new pg.Pool({
+  connectionString: 'postgres://dev:dev@localhost/studentGradeTable',
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+const app = express();
+app.use(express.json());
+
+app.get('/api/grades', async (req, res, next) => {
+  try {
+    const sql = `
+      select *
+      from "grades"
+    `;
+
+    const result = await db.query(sql);
+    const grades = result.rows;
+
+    res.status(200).json(grades);
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.use(errorMiddleware);
+
+app.listen(8080, () => {
+  console.log('Listening on port 8080');
+});
