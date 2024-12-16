fb07b5f1e0fd5288adc6d779e83988956675d170
Jesus Rodriguez-Luna <JesusRodriguezLuna3616@gmail.com>
2024-12-16 06:04:54 +0000

    Exercise pt.2 complete

--- express-crud/server.ts
+++ express-crud/server.ts
@@ -1,6 +1,6 @@
 import pg from 'pg';
 import express from 'express';
-import { errorMiddleware } from './lib';
+import { ClientError, errorMiddleware } from './lib';
 
 const db = new pg.Pool({
   connectionString: 'postgres://dev:dev@localhost/studentGradeTable',
@@ -16,13 +16,45 @@ app.get('/api/grades', async (req, res, next) => {
   try {
     const sql = `
       select *
-      from "grades"
+      from "grades";
     `;
 
     const result = await db.query(sql);
     const grades = result.rows;
 
     res.status(200).json(grades);
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.get('/api/grades/:gradeId', async (req, res, next) => {
+  try {
+    const { gradeId } = req.params;
+    if (
+      Number.isNaN(gradeId) ||
+      !Number.isInteger(+gradeId) ||
+      Number(gradeId) < 1
+    ) {
+      throw new ClientError(
+        400,
+        'gradeId must be an integer or greater than 0.'
+      );
+    }
+    const sql = `
+      select *
+      from "grades"
+      where "gradeId" = $1;
+    `;
+
+    const params = [gradeId];
+    const result = await db.query(sql, params);
+    const [grade] = result.rows;
+    if (!grade) {
+      throw new ClientError(404, `gradeId ${gradeId} does not exists.`);
+    }
+
+    res.status(200).json(grade);
   } catch (err) {
     next(err);
   }
