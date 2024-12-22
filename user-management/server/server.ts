e83598e913d6271b5349232ee6631a34e8149a47
Jesus Rodriguez-Luna <JesusRodriguezLuna3616@gmail.com>
2024-12-22 03:51:09 +0000

    Fixed sql to return 'createAt', 'userId', 'username'

--- user-management/server/server.ts
+++ user-management/server/server.ts
@@ -40,7 +40,7 @@ app.post('/api/auth/sign-up', async (req, res, next) => {
     const sql = `
       insert into "users" ("username", "hashedPassword")
       values($1, $2)
-      returning *;
+      returning "createdAt", "userId", "username";
     `;
 
     const param = [username, hashedPassword];
